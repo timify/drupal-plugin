a7149b7d464af5651703409df28d9d71248dc002
Deyan Spasov <deyan@e7studio.com>
2019-08-14 10:12:26 +0300

    Update plugins for the new TIMIFY

--- Drupal8/timify_widget/js/timify_widget.js
+++ Drupal8/timify_widget/js/timify_widget.js
@@ -5,17 +5,17 @@
       drupalSettings.timify_widget.id != null) {
 
     var script = document.createElement("script");
-    script.src = "https://widget.timify.com/js/widget.js";
+    script.src = "https://book.timify.com/widget/widget.min.js";
     script.setAttribute("async", "");
     script.setAttribute("id", "timify");
 
     if (typeof drupalSettings.timify_widget.position !== 'undefined' &&
         (drupalSettings.timify_widget.position == 'left' || drupalSettings.timify_widget.position == 'right')) {
 
-      script.setAttribute("data-id", drupalSettings.timify_widget.id);
+      script.setAttribute("data-account-id", drupalSettings.timify_widget.id);
     }
 
-    script.setAttribute("data-lang", drupalSettings.timify_widget.language);
+    script.setAttribute("data-locale", drupalSettings.timify_widget.language);
     script.setAttribute("data-position", (drupalSettings.timify_widget.position == 'left' || drupalSettings.timify_widget.position == 'right' ?
                                           drupalSettings.timify_widget.position : 'multiple'));
 
